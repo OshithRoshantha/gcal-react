69fef9975da1d5066f0557d3ee7599cef5d16551
Oshith Roshantha <oedirisuriya@gmail.com>
2024-05-28 18:07:01 +0530

    home page added

---
+++ src/Components/CalculateCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './CalculateCard.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export default function CalculateCard() {
+  return (
+    <div className='cardContainer'>
+    <Card style={{ width: '18rem' }} className='card' data-bs-theme="dark">
+      <Card.Img className='cardImg' variant="top" src="./cardIcon.png" />
+      <Card.Body className='cardBody'>
+        <Card.Title className='cardHead'>Calculate GPA</Card.Title>
+        <Card.Text className='cardText'>
+          Figure out your overall GPA, and how you stack up against the national average.
+        </Card.Text>
+        <Button className='goBtn'><img src='./go.png' className='goImg'/></Button>
+      </Card.Body>
+    </Card>
+    </div>
+  )
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Home.css'
+import CalculateCard from '../Components/CalculateCard'
+
 
 export default function Home() {
   return (
-    <div>
-        <h1>HomePage!</h1>
+    <div className='homeContainer'>
+      <div className='homeHeading'>
+        <img src='logo.png' className='homeIcon'/>
+      </div> 
+      <div className='homeHeading2'>
+          <br/>Calculate your Cumulative GPA, check your grades and understand how the GPA scale works.
+      </div>
+      <br/>
+      <CalculateCard/> 
     </div>
   )
 }
